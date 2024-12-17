fdd2ca3180c0aa360ff37949075d0d4a2c884c9a
Alec Wysoker <alecw@broadinstitute.org>
2024-12-17 16:56:28 -0500

    Change package name

--- R/SvmNucleusCaller-data.R
+++ R/SvmNucleusCaller-data.R
@@ -1,17 +1,17 @@
 # MIT License
-# 
+#
 # Copyright 2024 Broad Institute
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -38,7 +38,7 @@
 #'
 #' @examples
 #' data(svmNucleusCallerInputs)
-#' svmNucleusCaller = Dropseq.nucleiselection::SvmNucleusCaller(svmNucleusCallerInputs$cellFeatures,
+#' svmNucleusCaller = DropSift::SvmNucleusCaller(svmNucleusCallerInputs$cellFeatures,
 #' svmNucleusCallerInputs$dgeMatrix, datasetName = "v3_Bamboo_18d_10X_RNAseq_Optiprep8000_CaH_17k_rxn1",
 #' useCBRBFeatures = FALSE)
 #'
